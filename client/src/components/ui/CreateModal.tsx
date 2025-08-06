c0e5ecb608dda41f1d8d03e31b38fe407aa97de8
corey clarke <sombas100@yahoo.com>
2025-08-06 20:39:56 +0100

    changes

--- client/src/components/ui/CreateModal.tsx
+++ client/src/components/ui/CreateModal.tsx
@@ -31,7 +31,7 @@ const CreateProjectModal: FC<Props> = ({
     setError("");
 
     try {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem("freelancerToken");
       await client.post(
         "/projects/create",
         {
